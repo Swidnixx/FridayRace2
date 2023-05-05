d21a26fca27eb3c11aed6e05c4ef2d15b26acf2b
Swidnixx <swidnixx@gmail.com>
2023-05-05 20:07:23 +0200

    Paraca przy UI menu glownego. Zapisalismy zebrane dane do prefsow, ale color nie do konca dziala

---
+++ Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    public TextMeshProUGUI nicknameText;
+    public Renderer carRenderer;
+
+    public Slider sliderR;
+    public Slider sliderG;
+    public Slider sliderB;
+
+    public Button playButton;
+
+    private void Start()
+    {
+        sliderR.onValueChanged.AddListener(OnColorChanged);
+        sliderG.onValueChanged.AddListener(OnColorChanged);
+        sliderB.onValueChanged.AddListener(OnColorChanged);
+
+        sliderR.value = PlayerPrefs.GetFloat("R");
+        sliderG.value = PlayerPrefs.GetFloat("G");
+        sliderB.value = PlayerPrefs.GetFloat("B");
+        OnColorChanged(0);
+
+        nicknameText.text = PlayerPrefs.GetString("Nickname");
+        if(nicknameText.text == "")
+        {
+            playButton.interactable = false;
+        }
+    }
+
+    private void OnColorChanged(float value)
+    {
+        Color color = new Color(sliderR.value, sliderG.value, sliderB.value);
+        carRenderer.material.color = color;
+        nicknameText.color = color;
+
+        PlayerPrefs.SetFloat("R", color.r);
+        PlayerPrefs.SetFloat("G", color.g);
+        PlayerPrefs.SetFloat("B", color.b);
+    }
+
+    public void SetNickname(string nickname)
+    {
+        nicknameText.text = nickname;
+        PlayerPrefs.SetString("Nickname", nickname);
+
+        if (nicknameText.text == "")
+        {
+            playButton.interactable = false;
+        }
+        else
+        {
+            playButton.interactable = true;
+        }
+    }
+}
